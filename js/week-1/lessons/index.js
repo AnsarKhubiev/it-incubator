3a6eafb57ded397fefb0abd5a11f0183cbb8c4a5
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-16 23:21:41 +0300

    first commit

---
+++ js/week-1/lessons/index.js
@@ -0,0 +1,9 @@
+let dayOfWeek = "Sunday"
+
+switch (true) {
+   case dayOfWeek === "Saturday" || dayOfWeek === "Sunday":
+      console.log("Выходной день");
+      break;
+   default:
+      console.log("Будний день");
+}
