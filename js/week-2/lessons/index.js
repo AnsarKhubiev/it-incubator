948504cf63be75dc894e428457732a9b2bce0c75
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-22 15:36:49 +0300

    test

--- js/week-2/lessons/index.js
+++ js/week-2/lessons/index.js
@@ -4,14 +4,20 @@
 
 // showMe("Hellow", 1223);
 
-function showMe(val = 'Hellow Yullia') {
-   console.log(val);
-};
-
-showMe('Hellow Jenya');
-
-const showMe2 =  (val = 'Hellow Yullia') => {
-   console.log(val);
+function showMe(val = "Hellow Yullia") {
+    console.log(val);
 }
 
+showMe("Hellow Jenya");
+
+const showMe2 = (val = "Hellow Yullia") => {
+    console.log(val);
+};
+
 showMe2();
+
+const sum = (numb1, numb2) => {
+    console.log(numb1 + numb2);
+};
+
+sum(100, 200);
