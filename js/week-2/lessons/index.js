0c8f7a4257d848668d3bfd4d540d4ffbbd7a23b3
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-22 15:49:37 +0300

    test

--- js/week-2/lessons/index.js
+++ js/week-2/lessons/index.js
@@ -21,3 +21,21 @@ const sum = (numb1, numb2) => {
 };
 
 sum(100, 200);
+
+const sum2 = (numb1, numb2) => console.log(numb1 + numb2);
+
+const sum3 = numb2 => console.log(numb2);
+
+sum3(100);
+
+
+const isPositive = numb => {
+    if (numb >= 0) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+};
+
+isPositive(5);
+
