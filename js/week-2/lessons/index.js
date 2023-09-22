331e0467a7711f30ec76d44a83a98404aada193b
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-22 15:28:46 +0300

    lesson-2 JS

---
+++ js/week-2/lessons/index.js
@@ -0,0 +1,17 @@
+// function showMe(value, number) {
+//    console.log(value, number);
+// };
+
+// showMe("Hellow", 1223);
+
+function showMe(val = 'Hellow Yullia') {
+   console.log(val);
+};
+
+showMe('Hellow Jenya');
+
+const showMe2 =  (val = 'Hellow Yullia') => {
+   console.log(val);
+}
+
+showMe2();
