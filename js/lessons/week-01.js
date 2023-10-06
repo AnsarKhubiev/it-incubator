aaff808d495509676a16fcdd52ae30b11565ae89
AnsarKhubiev <anskhubiev@icloud.com>
2023-10-06 23:32:46 +0300

    test

--- js/lessons/week-01.js
+++ js/lessons/week-01.js
@@ -8,8 +8,6 @@
 //       console.log("Будний день");
 // }
 
-
-
 // toJadenCase = function () {
 //     return str.split(" ").map((el) => {
 //         return el.charAt(0).toUpperCase() + el.slice(1);
@@ -18,15 +16,20 @@
 
 // console.log(toJadenCase(str));
 
-
-
 // String.prototype.toJadenCase = function () {
 //     return this.split(" ").map((el) => {
 //         return el.charAt(0).toUpperCase() + el.slice(1);
 //     }).join(" ");
 // };
 
-
 // const str = "How can mirrors be real if our eyes aren't real";
 // arr = str.split(" ")
 // console.log(arr); //['How', 'can', 'mirrors', 'be', 'real', 'if', 'our', 'eyes', "aren't", 'real']
+
+const arr = [1,2,"aasf","1","123",123];
+typeof arr;
+
+function filter_list(arr) {
+    return arr.filter((el) => typeof(el) === 'number');
+}
+console.log(filter_list(arr));
